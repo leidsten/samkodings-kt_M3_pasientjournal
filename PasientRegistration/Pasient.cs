25dead53e8c2b9cff5996c87af845a51849b4d69
leidsten <linn@getacademy.no>
2022-09-16 13:26:46 +0200

    -

---
+++ PasientRegistration/Pasient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasientRegistration
+{
+    internal class Pasient
+    {
+        private string Name { get; set; }
+        public string Illness { get; set; }
+
+
+        public Pasient (string name, string illness)
+        {
+            Name = name;
+            Illness = illness;
+        }
+
+      
+        public string PrintInfo()
+        {
+            return Name + " " + Illness;
+        }
+    }
+}
---
+++ PasientRegistration/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace PasientRegistration
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // REGISTRERE PASIENT MED NAVN OG SYKDOM
+
+            //STEP 1:
+            //********* REGISTRERE EN PAS - U/KLASSE - SE PAS I CONSOLE ETTER REG ******
+            var pasientListe = new List<Pasient>();
+            var Frank = new Pasient("Frank", "Analkløe");
+            pasientListe.Add(Frank);
+
+            while(true)
+            {
+                Console.WriteLine("Please enter a name for the patient: ");
+                var patient = Console.ReadLine();
+                Console.WriteLine("Please enter illness: ");
+                var illness = Console.ReadLine();
+                var newPerson = new Pasient(patient, illness);
+                pasientListe.Add(newPerson);
+
+                //newPerson.PrintInfo();
+                // Console.WriteLine("Hei, " + patient + " Det var synd å høre at du sliter med: " + illness);
+
+                foreach (var pasient in pasientListe)
+                {
+                    Console.WriteLine(pasient.PrintInfo());
+                }
+
+
+            }
+            
+            //Console.ReloaodoooooooKooley
+
+            //STEP 2:
+            //********** REG FLER PAS - BRUKE KLASSE  **********
+
+
+
+
+
+
+
+
+            //STEP 2.1 - SE PAS.LISTE I CONSOLE - PUBLIC INFO I KLASSE
+
+
+
+
+
+            //STEP 2.2 - SE PAS.LISTE I CONSOLE - PRIVATE NAME I KLASSE
+        }
+    }
+}
